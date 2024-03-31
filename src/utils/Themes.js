0b9361c30ca586db6b371adab382decf116208a6
@Abirtijja <abirtijja5@gmail.com>
2024-03-31 18:25:42 +0200

    1 version

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#0D0D15",
+    bgLight: "#0D0F16",
+    primary:"#6A34E6",
+    text_primary:"#FDFEFF",
+    text_secondary:"#8E8F90",
+    card:"#0A0A10",
+    card_light: '#0C0C16',
+    button:"#6A34E6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FDFEFF",
+    bgLight: "#EFEFEF",
+    primary:"#8E12DB",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FDFEFF",
+    button:"#2C2B2B",
+}
